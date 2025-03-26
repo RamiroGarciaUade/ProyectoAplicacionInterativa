607263b7a1bf78b6241ffa97e650c887396920a3
Ramiro Garcia <ramiroigarcia.uade.edu.ar>
2025-03-26 14:55:00 -0300

    sentando las base del proyecto

---
+++ src/test/java/com/proyecto/uade/dieteticaYuyo/DieteticaYuyoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.uade.dieteticaYuyo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DieteticaYuyoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
