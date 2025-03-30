1128dc386204ced6a8f5cd1aabb59f9222638230
Tomas Basualdo <tomaa.basualdo@gmail.com>
2025-03-30 12:32:39 -0300

    3 entities added

---
+++ src/main/java/com/proyecto/uade/dieteticaYuyo/model/Product.java
@@ -0,0 +1,34 @@
+package com.proyecto.uade.dieteticaYuyo.model;
+
+import java.util.Locale.Category;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import lombok.Data;
+
+
+@Data
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String description;
+
+    @Column(nullable = false)
+    private Double price;
+
+    @Column
+    private String image;
+
+    @OneToOne
+    @JoinColumn(name = "category_id", referencedColumnName = "id")
+    private Category category;
+}
